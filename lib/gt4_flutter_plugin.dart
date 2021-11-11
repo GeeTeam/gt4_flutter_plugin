4928e3cc42fbfc63b6390f59140e38517223b8e8
NikoXu <chaosvita@icloud.com>
2021-11-11 14:34:24 +0800

    Refactor plugin

--- lib/gt4_flutter_plugin.dart
+++ lib/gt4_flutter_plugin.dart
@@ -5,7 +5,7 @@ import 'package:flutter/services.dart';
 typedef EventHandler = Function(Map<String, dynamic> event);
 
 class Gt4FlutterPlugin {
-  static const String flutterLog = "| Geetest | Flutter | ";
+  static const String flutterLog = "| Geetest 4.0 | Flutter | ";
   static const MethodChannel _channel = MethodChannel('gt4_flutter_plugin');
 
   static String get version {
@@ -17,17 +17,43 @@ class Gt4FlutterPlugin {
     return version;
   }
 
-  EventHandler? _onSuccess;
-  EventHandler? _onFailure;
+  EventHandler? _onShow;
+  EventHandler? _onResult;
+  EventHandler? _onError;
 
-  /// 使用注册参数开启验证
-  void verifyWithCaptcha(String captchaId) {
+  Gt4FlutterPlugin(String captchaId, [Map<String, String>? config]) {
     try {
-      _channel.invokeMethod('verifyWithCaptcha', {'captchaId': captchaId});
+      _channel.invokeMethod('initWithCaptcha', {'captchaId': captchaId, 'config': config}..removeWhere((key, value) => value == null));
     } catch (e) {
       print(flutterLog + e.toString());
     }
   }
+
+  /// 开启验证
+  void verify() {
+    try {
+      _channel.invokeMethod('verify');
+    } catch (e) {
+      print(flutterLog + e.toString());
+    }
+  }
+
+  void close() {
+    try {
+      _channel.invokeMethod('close');
+    } catch (e) {
+      print(flutterLog + e.toString());
+    }
+  }
+
+  // /// 使用注册参数开启验证
+  // void verifyWithCaptcha(String captchaId) {
+  //   try {
+  //     _channel.invokeMethod('verifyWithCaptcha', {'captchaId': captchaId});
+  //   } catch (e) {
+  //     print(flutterLog + e.toString());
+  //   }
+  // }
 
   void configurationChanged(Object object){
     try {
@@ -49,31 +75,40 @@ class Gt4FlutterPlugin {
     /// 失败结构示例:
     /// {response: {"captchaId":"647f5ed2ed8acb4be36784e01556bb71","captchaType":"slide",
     /// "challenge":"d04423f3-5297-44f5-bafa-cb868095c605"}, state: false}
-    EventHandler? onSuccess,
+    EventHandler? onResult,
 
     /// 错误回调
     /// 结构示例：{msg: 验证会话已取消, code: -14460, desc: {"description":"User cancelled 'Captcha'"}}
     /// 需要根据端类型区别处理错误码
     /// Android: https://docs.geetest.com/gt4/apirefer/errorcode/android
     /// iOS: https://docs.geetest.com/gt4/apirefer/errorcode/ios
-    EventHandler? onFailure,
+    EventHandler? onError,
+
+    ///
+    ///
+    ///
+    EventHandler? onShow,
   }) {
     print(flutterLog + "addEventHandler");
 
-    _onSuccess = onSuccess;
-    _onFailure = onFailure;
+    _onShow = onShow;
+    _onResult = onResult;
+    _onError = onError;
     _channel.setMethodCallHandler(_handler);
   }
 
   /// 原生回调
   Future<dynamic> _handler(MethodCall methodCall) async {
     switch (methodCall.method) {
-      case "onSuccess":
-        print(flutterLog + "onSuccess:" + _onSuccess.toString());
-        return _onSuccess!(methodCall.arguments.cast<String, dynamic>());
-      case "onFailure":
-        print(flutterLog + "onFailure:" + _onFailure.toString());
-        return _onFailure!(methodCall.arguments.cast<String, dynamic>());
+      case "onShow":
+        print(flutterLog + "onShow:" + _onShow.toString());
+        return _onShow!(methodCall.arguments.cast<String, dynamic>());
+      case "onResult":
+        print(flutterLog + "onResult:" + _onResult.toString());
+        return _onResult!(methodCall.arguments.cast<String, dynamic>());
+      case "onError":
+        print(flutterLog + "onError:" + _onError.toString());
+        return _onError!(methodCall.arguments.cast<String, dynamic>());
       default:
         throw UnsupportedError(flutterLog + "Unrecognized Event");
     }
