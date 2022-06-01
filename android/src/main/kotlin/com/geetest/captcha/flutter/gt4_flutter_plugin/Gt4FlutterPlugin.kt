64acd5c634aeda0fea2b5873a8ce70000b1e87f8
hexunyou <hexunyou@geetest.com>
2022-06-01 16:07:39 +0800

    add config for Android plugin

--- android/src/main/kotlin/com/geetest/captcha/flutter/gt4_flutter_plugin/Gt4FlutterPlugin.kt
+++ android/src/main/kotlin/com/geetest/captcha/flutter/gt4_flutter_plugin/Gt4FlutterPlugin.kt
@@ -3,8 +3,10 @@ package com.geetest.captcha.flutter.gt4_flutter_plugin
 import android.app.Activity
 import android.content.Context
 import android.content.res.Configuration
+import android.util.Log
 import androidx.annotation.NonNull
 import com.geetest.captcha.GTCaptcha4Client
+import com.geetest.captcha.GTCaptcha4Config
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
@@ -23,6 +25,7 @@ class Gt4FlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     private var activity: Activity? = null
     private lateinit var channel: MethodChannel
     private var gtCaptcha4Client: GTCaptcha4Client? = null
+    private val tag = "| Geetest | Android | "
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "gt4_flutter_plugin")
@@ -30,9 +33,10 @@ class Gt4FlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     }
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        Log.i(tag, "receive method: " + call.method)
         when (call.method) {
             "initWithCaptcha" -> {
-                initWithCaptcha(activity!!, call.argument<String>("captchaId").toString())
+                initWithCaptcha(activity!!, call.arguments)
             }
             "verify" -> {
                 verifyWithCaptcha()
@@ -53,39 +57,97 @@ class Gt4FlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
         channel.setMethodCallHandler(null)
     }
 
-    private fun initWithCaptcha(context: Context, captchaId: String) {
-        gtCaptcha4Client = GTCaptcha4Client.getClient(context)
-                .init(captchaId)
+    private fun initWithCaptcha(context: Context, param: Any?) {
+        if (param !is Map<*, *>) {
+            Log.e(tag, "initWithCaptcha arguments is not Map")
+            return
+        }
+        Log.i(tag, "parma:$param")
+
+        if (param.containsKey("config")) {
+            val configBuilder = GTCaptcha4Config.Builder()
+            val configParams = param["config"] as Map<String, Any>
+            if (configParams.containsKey("resourcePath")) {
+                configBuilder.setResourcePath(configParams["resourcePath"] as String)
+            }
+            val hashMap = HashMap<String, Any>()
+            if (configParams.containsKey("protocol")) {
+                hashMap["protocol"] = configParams["protocol"] as String
+            }
+            if (configParams.containsKey("userInterfaceStyle")) {
+                hashMap["displayMode"] = configParams["userInterfaceStyle"] as Int
+            }
+            if (configParams.containsKey("backgroundColor")) {
+                val backgroundColorStr = configParams["backgroundColor"] as String
+                if (backgroundColorStr.length == 8) {
+                    hashMap["bgColor"] = String.format("#%s", backgroundColorStr)
+                } else {
+                    hashMap["bgColor"] = String.format("#FF%s", backgroundColorStr)
+                }
+                Log.i(tag, "bgColor:" + hashMap["bgColor"])
+            }
+            if (configParams.containsKey("debugEnable")) {
+                configBuilder.setDebug(configParams["debugEnable"] as Boolean)
+            }
+            if (configParams.containsKey("canceledOnTouchOutside")) {
+                configBuilder.setCanceledOnTouchOutside(configParams["canceledOnTouchOutside"] as Boolean)
+            }
+            if (configParams.containsKey("timeout")) {
+                configBuilder.setTimeOut(configParams["timeout"] as Int)
+            }
+            if (configParams.containsKey("language")) {
+                configBuilder.setLanguage(configParams["language"] as String)
+            }
+            if (configParams.containsKey("additionalParameter")) {
+                val additionalParameter: Map<String, Any> = configParams["additionalParameter"] as Map<String, Any>
+                additionalParameter.forEach{
+                    hashMap[it.key] = it.value
+                }
+            }
+            configBuilder.setParams(hashMap)
+            gtCaptcha4Client = GTCaptcha4Client.getClient(context)
+                .init(param["captchaId"] as String, configBuilder.build())
+        } else {
+            gtCaptcha4Client = GTCaptcha4Client.getClient(context).init(param["captchaId"] as String)
+        }
+
     }
 
     private fun verifyWithCaptcha() {
         gtCaptcha4Client
-                ?.addOnSuccessListener { status, response ->
-                    val jsonObject: JSONObject
-                    val valueMap = HashMap<String, Any>()
-                    try {
-                        jsonObject = JSONObject(response)
-                        val iterator: Iterator<String> = jsonObject.keys()
-                        var key: String
-                        var value: Any
-                        while (iterator.hasNext()) {
-                            key = iterator.next()
-                            value = jsonObject[key] as Any
-                            valueMap[key] = value
-                        }
-                    } catch (e: Exception) {
-                        e.printStackTrace()
+            ?.addOnSuccessListener { status, response ->
+                val jsonObject: JSONObject
+                val valueMap = HashMap<String, Any>()
+                try {
+                    jsonObject = JSONObject(response)
+                    val iterator: Iterator<String> = jsonObject.keys()
+                    var key: String
+                    var value: Any
+                    while (iterator.hasNext()) {
+                        key = iterator.next()
+                        value = jsonObject[key] as Any
+                        valueMap[key] = value
                     }
-                    channel.invokeMethod("onResult", hashMapOf("status" to if (status) "1" else "0", "result" to valueMap))
+                } catch (e: Exception) {
+                    e.printStackTrace()
                 }
-                ?.addOnFailureListener { message ->
-                    val jsonObject = JSONObject(message)
-                    channel.invokeMethod("onError",
-                            hashMapOf("code" to jsonObject.optString("code"),
-                                    "msg" to jsonObject.optString("msg"),
-                                    "desc" to jsonObject.optJSONObject("desc")?.toString()))
-                }
-                ?.verifyWithCaptcha()
+                channel.invokeMethod(
+                    "onResult",
+                    hashMapOf("status" to if (status) "1" else "0", "result" to valueMap)
+                )
+            }
+            ?.addOnFailureListener { message ->
+                val jsonObject = JSONObject(message)
+                channel.invokeMethod(
+                    "onError",
+                    hashMapOf(
+                        "code" to jsonObject.optString("code"),
+                        "msg" to jsonObject.optString("msg"),
+                        "desc" to jsonObject.optJSONObject("desc")?.toString()
+                    )
+                )
+            }
+            ?.verifyWithCaptcha()
     }
 
     private fun configurationChanged(newConfig: Configuration?) {
